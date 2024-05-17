62c44bff5e847f8626d0959bfd454244addfc779
Rea <rhasanaj@yahoo.com>
2024-05-18 00:35:46 +0530

    Assignment 5 solution

--- server/index.js
+++ server/index.js
@@ -5,6 +5,13 @@ const PORT = 3000
 const clientPath = path.resolve(__dirname, '../client/dist')
 app.use(express.static(clientPath))
 app.get('/', (req, res) => {
-    // we'll do some stuff here
     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
-  })
+})
+
+app.get('/test', (req, res) => {
+    res.send('Server is operational');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})  
