d3ccc4162915ac78d94cfa4d07c25e0a79dfee30
Rea <rhasanaj@yahoo.com>
2024-05-18 00:23:24 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import Post from "./components/Post"
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Fakebook!</h1>
+      {/* CreatePostForm */}
+      {/* Feed*/}
+      <Post content = "This is a test post!"/>
+      <Post content = "This is another test post!"/>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Post.jsx
@@ -0,0 +1,23 @@
+import Comment from "./Comment"
+import { useState } from "react"
+function Post(props){
+    const [likes, setLikes] = useState(0);
+
+    function handleLike(){
+        setLikes(likes+1);
+    }
+
+    return(
+        <div>
+            <h3>{props.content}</h3>
+            <p>Likes: {likes}</p>
+            <button onClick={handleLike}>Like</button>
+            <p>Comments: </p>
+            <Comment content = "This is a test comment!"/>
+            <Comment content = "This is another test comment!"/>
+            <Comment content = "This is yet another test comment!"/>
+        </div>
+    )
+}
+
+export default Post
