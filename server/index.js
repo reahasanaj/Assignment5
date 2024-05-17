e18ecf633513094755d7c476954d5f1ece5628c7
Rea <rhasanaj@yahoo.com>
2024-05-18 00:31:54 +0530

    build server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+const PORT = 3000
+const clientPath = path.resolve(__dirname, '../client/dist')
+app.use(express.static(clientPath))
+app.get('/', (req, res) => {
+    // we'll do some stuff here
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+  })
